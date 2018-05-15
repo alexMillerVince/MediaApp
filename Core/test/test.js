7893a5c5698b6977f74da2e467b753235f6cb927
Alex Miller <alex.miller.vince@gmail.com>
2018-05-15 10:37:20 +0200

    Add Test cases: Test, Databse: #addMovie()

--- Core/test/test.js
+++ Core/test/test.js
@@ -2,3 +2,19 @@
 
 const assert = require('assert');
 const db = require('../lib/db');
+
+describe('Test', function () {
+   describe('#firstTest', function () {
+       it('should return true', function () {
+           assert.equal(true, true);
+       });
+   })
+});
+
+describe('Database', function () {
+    describe('#addMovie()', function () {
+        it('should save to databse without any error', function (done) {
+            db.addMove('Test', 2019, 7, 'Action', 1, 30, 'Description', done);
+        });
+    })
+});
