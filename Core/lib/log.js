e5d5e3c905f98af516e5d09b3a2f33c129b5074c
Alex Miller <alex.miller.vince@gmail.com>
2018-05-17 13:17:10 +0200

    Add log.js

---
+++ Core/lib/log.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const console_log = true;
+const fs = require('fs');
+
+const log = function (input) {
+    const date = new Date();
+    if (input === '') {
+        input = '### END ###';
+    }
+    input = date.getDay().toString()+ '/' + date.getDate().toString() + '/'
+        + date.getFullYear().toString() + ' # ' + input;
+
+    fs.appendFile('log.log', input + '\r\n', () => {});
+    if (console_log) {
+        console.log(input);
+    }
+};
+
+module.exports = log;
