9ad8c8e435f6f920eb9fb1f3c6896534da869861
Alex Miller <alex.miller.vince@gmail.com>
2018-05-28 10:17:12 +0200

    Add app.js

--- Core/app.js
+++ Core/app.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const Hapi = require('hapi');
+const  nCore = require('./lib/ncore');
+const config = require('./config');
+
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+});
+
+const ncore = new nCore(config);
+
+server.route({
+    method : 'POST',
+    path : '/refresh',
+    handler : (request, reply) => {
+        ncore.start();
+
+        return request.payload;
+    }
+});
+
+const init = async () => {
+
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
